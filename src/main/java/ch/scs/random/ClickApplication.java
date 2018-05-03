56d6f0c6aa058d69144bd993c2b9fb0b74935389
Dmitry Logvinovich <adlerstone@yahoo.co.uk>
2018-05-03 10:22:58 +0200

    Application with bmp image

--- src/main/java/ch/scs/random/ClickApplication.java
+++ src/main/java/ch/scs/random/ClickApplication.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ClickApplication extends Application {
@@ -18,13 +20,21 @@ public class ClickApplication extends Application {
 	}
 
 	// scene object for unit tests
-	public static class ClickPane extends StackPane {
+	public static class ClickPane extends BorderPane {
 		public ClickPane() {
 			super();
 			Button button = new Button("click me!");
 			button.setId("button");
 			button.setOnAction(actionEvent -> button.setText("clicked!"));
-			getChildren().add(button);
+
+			setBottom(button);
+
+			ImageView imageView = new ImageView();
+			imageView.setId("imageView");
+			Image image = new Image(
+					getClass().getClassLoader().getResourceAsStream("Ausgabeschalter_angezogen_rechts.bmp"));
+			imageView.setImage(image);
+			setCenter(imageView);
 		}
 	}
 }
