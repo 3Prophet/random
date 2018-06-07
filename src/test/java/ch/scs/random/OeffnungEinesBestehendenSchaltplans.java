78cc42efa290098cd4234615ce661a070d575877
Dmitry Logvinovich <dmitry.logvinovich@scs.ch>
2018-06-07 14:00:30 +0200

    First complete logic implementation for OpenEinesBestehendenSchaltplans

--- src/test/java/ch/scs/random/OeffnungEinesBestehendenSchaltplans.java
+++ src/test/java/ch/scs/random/OeffnungEinesBestehendenSchaltplans.java
@@ -3,106 +3,65 @@ package ch.scs.random;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.concurrent.TimeoutException;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.testfx.api.FxRobot;
 
+import ch.scs.random.SchaltplanEditorRobot.FileOpenAction;
 import ch.scs.random.utils.FileChooserWrapper;
 
-public class OeffnungDesBestehendesBildesTestFall extends JavaFxApplicationTest<ImageEditor> {
+public class OeffnungEinesBestehendenSchaltplans extends JavaFxApplicationTest<ImageEditor> {
 
     @Mock
     private FileChooserWrapper chooser;
 
     @Override
+    @BeforeEach
     public void setUp() throws TimeoutException, InterruptedException {
 
         super.setUp();
         MockitoAnnotations.initMocks(this);
 
-        when(chooser.showOpenDialog(any()))
-                .thenReturn(new File("D:\\stw\\workspaces\\new\\random\\src\\main\\resources\\Ausgabeschalter_angezogen_rechts.bmp"));
-        ((ClickApplication) application).setFileChooser(chooser);
+        when(chooser.showOpenDialog(any())).thenReturn(TestResources.getTestSchaltplanFile());
+        ((ImageEditor) application).setFileChooser(chooser);
     }
 
-    Robot robot = null;
-
-    OeffnungDesBestehendesBildesTestFall() {
+    OeffnungEinesBestehendenSchaltplans() {
         super(ImageEditor.class);
-        robot = new Robot(super.fxRobot);
     }
 
-    private class Robot {
-
-        private FxRobot fxRobot = null;
-
-        public Robot(FxRobot fxRobot) {
-            this.fxRobot = fxRobot;
-        }
-
-        public void userInitiatesOpeningOfExistingSchaltplan() throws InterruptedException {
-            fxRobot.clickOn("#fileMenu")
-                    .clickOn("#openMenuItem");
-            Thread.sleep(2000);
-
-        }
-
-        public void schaltplanEditorRequestsPathAndFileNameOfExistingFile() {
-            // TODO Auto-generated method stub
-
-        }
-
-        public void userProvidesPathAndFileName() {
-            // TODO Auto-generated method stub
-
-        }
-
-        public void schaltplanEditorRequestsUserNameAndRole() {
-            // TODO Auto-generated method stub
-
-        }
-
-        public void userProvidesHisNameAndRoleToSchaltplanEditor() {
-            // TODO Auto-generated method stub
-
-        }
-
-        public void schaltplanEditorOpensRequestedSchaltplan() {
-            // TODO Auto-generated method stub
-
-        }
-
-        public void schaltplanEditorDisplaysNameAndPathOfTheRequestedSchaltplan() {
-            // TODO Auto-generated method stub
-
-        }
-
-        public void schaltplanEditorKeepsRoleAndNameEnteredByUser() {
-            // TODO Auto-generated method stub
-
-        }
+    private static Iterable<Object[]> actionRoleData() {
+        return Arrays.asList(new Object[][] { { SchaltplanEditorRobot.fileOpenViaMenuAction, "Planer" },
+                { SchaltplanEditorRobot.fileOpenViaMenuAction, "Planprüfer" }, { SchaltplanEditorRobot.fileOpenViaMenuAction, "Betrachter" },
+                { SchaltplanEditorRobot.fileOpenViaButtonAction, "Planer" }, { SchaltplanEditorRobot.fileOpenViaButtonAction, "Planprüfer" },
+                { SchaltplanEditorRobot.fileOpenViaButtonAction, "Betrachter" } });
     }
 
-    public static Iterable<Object[]> ausloeserRolleKombinationen() {
-        return Arrays.asList(new Object[][] { { "Ausgabeschalter_angezogen_rechts.bmp", -1L },
-                { "Aufloesevorbereiter_abgefallen_Kontakt_leitend_links.bmp", 0L } });
-    }
-
-    @Test
-    public void benutzerKannBestehendesBildOeffnen() throws InterruptedException {
-        robot.userInitiatesOpeningOfExistingSchaltplan();
+    /**
+     * Testfall: Oeffnen enies bestehenden Schaltplans.
+     * 
+     * @param action
+     *            Aktion, damit dieser Tesfall getriggert wird.
+     * @param userRole
+     *            Benutzer rolle.
+     * @throws InterruptedException
+     */
+    @ParameterizedTest(name = "[{index}] Action: {0}, Role: {1}")
+    @MethodSource("actionRoleData")
+    public void benutzerKannBestehendesBildOeffnen(FileOpenAction action, String userRole) {
+        robot.userInitiatesOpeningOfExistingSchaltplan(action);
         robot.schaltplanEditorRequestsPathAndFileNameOfExistingFile();
         robot.userProvidesPathAndFileName();
         robot.schaltplanEditorRequestsUserNameAndRole();
-        robot.userProvidesHisNameAndRoleToSchaltplanEditor();
+        robot.userProvidesHisNameAndRoleToSchaltplanEditor(userRole);
         robot.schaltplanEditorOpensRequestedSchaltplan();
         robot.schaltplanEditorDisplaysNameAndPathOfTheRequestedSchaltplan();
-        robot.schaltplanEditorKeepsRoleAndNameEnteredByUser();
+        robot.schaltplanEditorKeepsRoleAndNameEnteredByUser(userRole);
     }
 
 }
